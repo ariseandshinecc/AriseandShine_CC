cee5dbb5a822365daf558596db9cfb62ff54f46c
google-boy <otienoh13@gmail.com>
2021-02-26 22:03:04 +0300

    Add Source Code Form License Notice.

--- Arise_and_Shine_CBO/urls.py
+++ Arise_and_Shine_CBO/urls.py
@@ -1,3 +1,7 @@
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """Arise_and_Shine_CBO URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
--- mainsite/context_processors.py
+++ mainsite/context_processors.py
@@ -1,4 +1,11 @@
-# Context data available for all templates
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+""" 
+    Context data available for all templates 
+"""
 from .models import Project, Profile
 
 def project_renderer(request):
