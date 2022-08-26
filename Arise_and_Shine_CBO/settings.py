a5e31002a35f8969e9bf2a7649e12f8957169205
hezron <otienoh13@gmail.com>
2022-08-26 13:21:22 +0300

    Explicitly set DEFAULT_AUTO_FIELD to AutoField

--- Arise_and_Shine_CBO/settings.py
+++ Arise_and_Shine_CBO/settings.py
@@ -94,6 +94,8 @@ DATABASES = {
     )
 }
 
+# Auto-created primary key
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
